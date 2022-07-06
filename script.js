3eefb012edb67e84b4d2eca796ef138970e26915
Harman Randhawa <54494701+randhawah@users.noreply.github.com>
2022-07-05 23:54:46 -0400

    add rainbow, color picker and other functions

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
 const container = document.getElementById("container");
 var slider = document.getElementById("slider");
 var sizeResult = document.getElementById("sizeResult");
-var gridSize = 5;
+var rainbowBtn = document.getElementById("rainbow");
+var input = document.getElementById('colorPicker');
 var clearBtn = document.getElementById("clear");
-//var test = document.querySelectorAll(".test");
+var gridSize = 24;
+var colorPicker = "black";
+var rainbowTgl = false;
 
+//creates a default grid of size 24X24
 window.onload = createGrid(gridSize);
 
-/* This was a test case to test changing colors of multiple elements with same class
-on mouseover.
-
-for(let i=0; i<test.length; i++){
-  test[i].addEventListener("mouseover", function(){
-      test[i].style.setProperty('--boxColor', "black");
-  }, false);
-}
-*/
-
+//creates the grid of 'size' and set the CSS Grid rows and columns to 'size'
 function createGrid(size) {
   container.style.setProperty('--size', size);
   container.textContent = "";
-
   for (let i = 0; i < (size * size); i++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
@@ -28,8 +22,11 @@ function createGrid(size) {
   var box = document.getElementsByClassName("grid-item");
   for(let i = 0; i<box.length; i++){
     box[i].addEventListener("mouseover", function(){
-      box[i].style.setProperty('--boxColor', "black");
-  }, false);
+      if(rainbowTgl)
+        box[i].style.setProperty('--boxColor', createRainbow());
+      else
+        box[i].style.setProperty('--boxColor', colorPicker);
+    }, false);
   }
 };
 
@@ -39,8 +36,43 @@ slider.addEventListener("change", function(){
     createGrid(gridSize);
 }, false);
 
+function getData(color){
+  container.style.setProperty('--colorPicker', color);
+  if(rainbowTgl)rainbowToggle();
+  colorPicker = color;
+}
+
+function rainbowToggle(){
+  if(rainbowTgl == false){
+    rainbowTgl = true;
+    rainbowBtn.style.setProperty('--rainbow-btn-color', "green");
+    rainbowBtn.innerText = 'Rainbow ON!';
+    colorPicker = createRainbow();
+  }
+  else if(rainbowTgl == true){
+    rainbowTgl = false;
+    rainbowBtn.style.setProperty('--rainbow-btn-color', "red");
+    rainbowBtn.innerText = 'Rainbow OFF!';
+  }
+}
+
+function createRainbow(){
+  return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
 clearBtn.addEventListener("click", function(){
   box = document.getElementsByClassName("grid-item");
   for(let i =0; i<box.length; i++)
     box[i].style.setProperty('--boxColor', "white");
 }, false);
+
+
+// when you click the color picker, we'll run our code to close it after 2 seconds.
+input.addEventListener('focus', function() {
+  setTimeout(()=>{
+    // toggle the type attribute to close the picker!
+    input.setAttribute('type','text');
+    input.setAttribute('type','color');
+    
+  }, 2000);
+});
